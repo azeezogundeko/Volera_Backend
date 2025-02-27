d7424f7fbd35eacf92d83fb4b722649462226c0c
Engr Ogundeko <azeezogundeko19@gmail.com@example.com>
2025-02-27 22:04:05 +0100

    fixed tracking error

--- utils/celery_tasks.py
+++ utils/celery_tasks.py
@@ -57,21 +57,21 @@ celery_app.conf.update(
                 }
             }
         },
-        'track-prices-startup': {
-            'task': 'price_tracking.schedule_price_tracking',
-            'schedule': 0.0,  # Run immediately after startup
-            'options': {
-                'queue': 'default',
-                'expires': 3600,
-                'retry': True,
-                'retry_policy': {
-                    'max_retries': 3,
-                    'interval_start': 0,
-                    'interval_step': 0.2,
-                    'interval_max': 0.2,
-                }
-            }
-        }
+        # 'track-prices-startup': {
+        #     'task': 'price_tracking.schedule_price_tracking',
+        #     'schedule': 0.0,  # Run immediately after startup
+        #     'options': {
+        #         'queue': 'default',
+        #         'expires': 3600,
+        #         'retry': True,
+        #         'retry_policy': {
+        #             'max_retries': 3,
+        #             'interval_start': 0,
+        #             'interval_step': 0.2,
+        #             'interval_max': 0.2,
+        #         }
+        #     }
+        # }
     }
 )
 
